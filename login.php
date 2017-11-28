727921ef0124a6b0004a5fa1150057628fa95e35
Burns-R-BPC-Magenta <32098227+Burns-R-BPC-Magenta@users.noreply.github.com>
2017-11-28 13:51:34 +0000

    Delete login.php

--- login.php
+++
@@ -1,7 +0,0 @@
-<html>
-<form action = "login.php" method = "post">
-Username <input type="text" name="username">
-Password <input type="password" name="password">
-<input type="submit" value="Login">
-</form>
-
