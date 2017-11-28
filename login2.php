fafd3e5a82416325a88709236b515a5964826d71
Burns-R-BPC-Magenta <32098227+Burns-R-BPC-Magenta@users.noreply.github.com>
2017-11-28 13:51:51 +0000

    Delete login2.php

--- login2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$uname = $_POST['username'];
-$pass = $_POST['password'];
-
-if($uname == "Robbie" && $pass == "123")
-	echo "Content after login has been successful.";
-else
-	echo "invalid";
-?>
